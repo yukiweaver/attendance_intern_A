14c79a671feb3cf94ccc8c7d01de2f13ca041f87
Yuki Ueno <yuki.weaver1204@gmail.com>
2018-10-29 11:45:21 +0000

    8.1.5complete

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,5 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  post '/signup',  to: 'users#create'   #7.3.4.2で追加
   resources :users
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,15 +5,13 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do      #無効なユーザー登録に対するテスト
     get signup_path
     assert_no_difference 'User.count' do
-      post signup_path, params: { user: { name:  "",              #7.3.4.3で一部変更
+      post users_path, params: { user: { name:  "",              
                                          email: "user@invalid",
                                          password:              "foo",
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
-    assert_select 'div#error_explanation'   #演習7.3.4.1で追加
-    assert_select 'div.alert'               #演習7.3.4.1で追加
-    assert_select 'form[action="/signup"]'  #演習7.3.4.4で追加
+    
   end
   
   test "valid signup information" do        #有効なユーザー登録に対するテスト
