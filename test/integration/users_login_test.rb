108ce5298011c1a8ad70672f9cd92caaf64a1004
Yuki Ueno <yuki.weaver1204@gmail.com>
2018-10-30 08:17:05 +0000

    8.2.4complete

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+  
+  #フラッシュメッセージの残留をキャッチするテスト
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
@@ -10,5 +15,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+  
+  #有効な情報を使ってユーザーログインをテストする
+  test "login with valid information" do
+    get login_path
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
