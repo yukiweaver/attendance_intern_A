b02a0015313103df7f61859ccc0974157e9278ed
Yuki Ueno <yuki.weaver1204@gmail.com>
2018-10-31 08:15:23 +0000

    9.3.1complete

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -45,11 +45,27 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
-    # 2番目のウィンドウでログアウトをクリックするユーザーをシミュレートする
+    # 2番目のウィンドウでログアウトをクリックするユーザーをシミュレートする リスト9.14
     delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
+  
+  #リスト 9.25: [remember me] チェックボックスのテスト
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies['remember_token']
+  end
+  
+  #リスト 9.25: [remember me] チェックボックスのテスト
+  test "login without remembering" do
+    # クッキーを保存してログイン
+    log_in_as(@user, remember_me: '1')
+    delete logout_path
+    # クッキーを削除してログイン
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,12 +7,25 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-  include ApplicationHelper
+  include ApplicationHelper     #リスト9.24で消えている...とりあえずこのまま
   
   # テストユーザーがログイン中の場合にtrueを返す
   def is_logged_in?
     !session[:user_id].nil?
   end
 
-  # Add more helper methods to be used by all tests here...
+  # テストユーザーとしてログインする  リスト 9.24: log_in_asヘルパーを追加
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+
+  # テストユーザーとしてログインする  リスト 9.24: log_in_asヘルパーを追加
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
 end
