af2c9bddbb7e960473dd51a2f6cbc93528a7154e
Yuki Ueno <yuki.weaver1204@gmail.com>
2018-10-31 10:56:50 +0000

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,7 +12,7 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
   
-  # 記憶トークンcookieに対応するユーザーを返す リスト 9.9: 永続的セッションのcurrent_userを更新
+  # 記憶トークンcookieに対応するユーザーを返す
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -25,13 +25,6 @@ module SessionsHelper
     end
   end
 
-  # 現在ログイン中のユーザーを返す (いる場合)
-  def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
-  end
-  
   # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !current_user.nil?
