8928906553af353288328c51ed7bf28d5c0afe47
Yuki Ueno <yuki.weaver1204@gmail.com>
2018-11-01 14:16:34 +0000

    10.1.3complete

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+  
+  #リスト 10.9: 編集の失敗に対するテスト
+  test "unsuccessful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { name:  "",
+                                              email: "foo@invalid",
+                                              password:              "foo",
+                                              password_confirmation: "bar" } }
+
+    assert_template 'users/edit'
+  end
+end
