914c65957a59af351f57ed34a07564157602171f
Yuki Ueno <yuki.weaver1204@gmail.com>
2018-11-05 09:11:33 +0000

    10.3.1complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]   #リスト 10.15
+  before_action :logged_in_user, only: [:index, :edit, :update]   #リスト 10.15 リスト 10.35
   before_action :correct_user,   only: [:edit, :update]   #リスト 10.25
+  
+  #リスト 10.35 リスト 10.36
+  def index
+    @users = User.all
+  end
   
   def show
     @user = User.find(params[:id])
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,6 +6,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @other_user = users(:archer)    #リスト 10.24
   end
   
+  #リスト 10.34: indexアクションのリダイレクトをテスト
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+  
   test "should get new" do
     get signup_path
     assert_response :success
