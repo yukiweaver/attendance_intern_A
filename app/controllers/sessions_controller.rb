494c1063be95fc7e95abdb6c4b855f9ad95601de
Yuki Ueno <yuki.weaver1204@gmail.com>
2018-11-06 14:21:27 +0000

    11.3.2complete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)  #リスト 9.23: [remember me] チェックボックスの送信結果を処理
-      redirect_back_or user   #リスト 10.32: フレンドリーフォワーディングを備える
+      if user.activated?    #リスト 11.32: 有効でないユーザーがログインすることのないようにする
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)  #リスト 9.23: [remember me] チェックボックスの送信結果を処理
+        redirect_back_or user   #リスト 10.32: フレンドリーフォワーディングを備える
+      else
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
