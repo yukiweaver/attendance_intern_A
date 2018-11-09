04dd1d9424cbc60b51c465773e26f52fe77a771d
Yuki Ueno <yuki.weaver1204@gmail.com>
2018-11-09 11:58:33 +0000

    13.1.2complete

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,6 @@
 #リスト 13.2: 自動生成されたMicropostモデル
 class Micropost < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true    #リスト 13.5
+  validates :content, presence: true, length: { maximum: 140 }    #リスト 13.8
 end
--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:michael)
+    # このコードは慣習的に正しくない
+    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+  
+  #リスト 13.4: 新しいMicropostの有効性に対するテスト
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+  
+  #リスト 13.4: 新しいMicropostの有効性に対するテスト
+  test "user id should be present" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+  
+  #リスト 13.7: Micropostモデルのバリデーションに対するテスト
+  test "content should be present" do
+    @micropost.content = "   "
+    assert_not @micropost.valid?
+  end
+  
+  #リスト 13.7: Micropostモデルのバリデーションに対するテスト
+  test "content should be at most 140 characters" do
+    @micropost.content = "a" * 141
+    assert_not @micropost.valid?
+  end
 end
