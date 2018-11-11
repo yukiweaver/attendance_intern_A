d682a649716cc231f3722f858a136a175a1e9cfd
Yuki Ueno <yuki.weaver1204@gmail.com>
2018-11-11 12:59:55 +0000

    13.4.2complete

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,4 +5,14 @@ class Micropost < ApplicationRecord
   mount_uploader :picture, PictureUploader    #リスト 13.59: Micropostモデルに画像を追加
   validates :user_id, presence: true    #リスト 13.5
   validates :content, presence: true, length: { maximum: 140 }    #リスト 13.8
+  validate  :picture_size   #リスト 13.65: 画像に対するバリデーションを追加
+  
+  private
+
+    # リスト 13.65:アップロードされた画像のサイズをバリデーションする
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
