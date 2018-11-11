39fe54a23af50168ae3e979926cca6c7f3ca29cd
Yuki Ueno <yuki.weaver1204@gmail.com>
2018-11-11 09:43:41 +0000

    13.3.4complete

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
   #リスト 13.34: Micropostsコントローラの各アクションに認可を追加
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy   #リスト 13.52
   
   #リスト 13.36: Micropostsコントローラのcreateアクション
   def create
@@ -14,13 +15,23 @@ class MicropostsController < ApplicationController
       render 'static_pages/home'
     end
   end
-
+  
+  #リスト 13.52: Micropostsコントローラのdestroyアクション
   def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    redirect_to request.referrer || root_url
   end
 
   private
 
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+    
+    #リスト 13.52
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
     end
 end
