f71d0c148e44eb49e8ae9bd769e4b1467ff37983
Yuki Ueno <yuki.weaver1204@gmail.com>
2018-11-20 09:28:52 +0000

    ログイン後の表示多数２

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -13,10 +13,10 @@ class PasswordResetsController < ApplicationController
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
-      flash[:info] = "Email sent with password reset instructions"
+      flash[:info] = "メールを送信しました。"
       redirect_to root_url
     else
-      flash.now[:danger] = "Email address not found"
+      flash.now[:danger] = "メールアドレスが間違っています。"
       render 'new'
     end
   end
@@ -31,7 +31,7 @@ class PasswordResetsController < ApplicationController
       render 'edit'
     elsif @user.update_attributes(user_params)          # (4) への対応
       log_in @user
-      flash[:success] = "Password has been reset."
+      flash[:success] = "パスワードを再設定しました。"
       redirect_to @user
     else
       render 'edit'                                     # (2) への対応
