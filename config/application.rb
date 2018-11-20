10cb7ac0155a7a1047fe21c5021092039984d56c
Yuki Ueno <yuki.weaver1204@gmail.com>
2018-11-20 14:06:49 +0000

    日本語化１

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,15 @@ module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    
+    #ポートフォリオで追加
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    #　ポートフォリオ：以下の記述を追記する(設定必須)
+    config.i18n.default_locale = :ja # デフォルトのlocaleを日本語(:ja)にする
+    #　#　ポートフォリオ：以下の記述を追記する(設定必須)
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
