876de82ded75f571f5a894716d865c7b1669510b
EC2 Default User <ec2-user@ip-172-31-6-94.us-east-2.compute.internal>
2018-12-30 12:09:08 +0000

    新規登録後ログイン状態へ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       #@user.send_activation_email    #リスト 11.23: ユーザー登録にアカウント有効化を追加→リスト 11.36で変更
       #flash[:info] = "メールを確認してアカウントを有効にしてください。"
       flash[:info] = "新規登録しました。"
