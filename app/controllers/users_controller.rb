d0d56dc1a050d705fc89cd1e5688bbe88e5c65db
EC2 Default User <ec2-user@ip-172-31-6-94.us-east-2.compute.internal>
2019-01-07 14:24:04 +0000

    時間管理表取得2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])    #リスト 13.23
-    @current_day = Date.today
+    @current_day = Date.today  #勤怠B：現在の年月日を取得
+    @last_month = @current_day.last_month
+    @next_month = @current_day.next_month
     #@current_day = Date.today.strftime("%Y")
     #@current_day2 = Date.today.strftime("%m")
   end
