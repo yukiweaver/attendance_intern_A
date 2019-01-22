d5fd721b13b3933962d16f4e22889e6d04cb1df5
EC2 Default User <ec2-user@ip-172-31-6-94.us-east-2.compute.internal>
2019-01-22 12:33:10 +0000

    勤怠編集ページ2

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -11,5 +11,19 @@ class AttendancesController < ApplicationController
     @next_month = @current_day.next_month
     @first_day = @current_day.beginning_of_month
     @last_day = @current_day.end_of_month
+    @week = %w(日 月 火 水 木 金 土 )
+    
+    (@first_day..@last_day).each do |d|
+      if not @user.attendances.any?{|a| a.attendance_day == d}
+        @attendance1 = @user.attendances.build(attendance_day: d)
+        @attendance1.save
+      end
+      @date = @user.attendances.where("attendance_day >= ? and attendance_day <= ?", @first_day, @last_day)
+    end
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @attendance = Attendance.find(paramas:[:id])
   end
 end
