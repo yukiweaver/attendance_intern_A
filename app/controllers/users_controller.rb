4cf3b989b351d115dc21765538a4c081f1971b12
EC2 Default User <ec2-user@ip-172-31-6-94.us-east-2.compute.internal>
2019-01-22 06:35:03 +0000

    総合勤務時間表示3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,9 +54,9 @@ class UsersController < ApplicationController
     @date.each do |date|
       if date.beginning_time != nil && date.leaving_time != nil
         @company_time = date.leaving_time - date.beginning_time
-        @start_company_time = 0
-        @total_company_time = (@start_company_time + @company_time)/3600
-        @attendance_count = @user.attendances.where("beginning_time != ? or leaving_time != ?", nil?, nil?).count
+        #@start_company_time = 0
+        @total_company_time = (@total_company_time.to_f+ @company_time)
+        @attendance_count = @user.attendances.where("beginning_time != ? and leaving_time != ?", nil?, nil?).count
       end
     end
   end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -20,6 +20,10 @@ module UsersHelper
   
   # 勤怠B：勤怠表示画面 在社時間の合計 フォーマットヘルパー使用→指定の桁で丸める
   def total_company_edit(time)
-    number_with_precision time, precision:2
+    number_with_precision time/3600, precision:2
+  end
+  
+  def comprehensive_working_times(time)
+    format("%.2f", time) if !time.blank?
   end
 end
