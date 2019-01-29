7442317d4e3e29e22f7913920ca06cc3d0bc0ceb
EC2 Default User <ec2-user@ip-172-31-6-94.us-east-2.compute.internal>
2019-01-29 14:26:25 +0000

    勤怠編集更新12

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -6,31 +6,38 @@ class AttendancesController < ApplicationController
   # 勤怠B：勤怠編集ページ
   def attendance_edit
     @user = User.find(params[:id])
-    #@attendance = Attendance.find(params[:id])
-    @current_day = Date.new(Date.today.year, Date.today.month, Date.today.day)
-    @last_month = @current_day.last_month
-    @next_month = @current_day.next_month
-    @first_day = @current_day.beginning_of_month
-    @last_day = @current_day.end_of_month
-    @week = %w(日 月 火 水 木 金 土 )
     
-    (@first_day..@last_day).each do |d|
-      if not @user.attendances.any?{|a| a.attendance_day == d}
-        #@attendance = Attendance.new(attendance_day: d, user_id: @user.id) 以下の@attendanceと同意味
-        @attendance1 = @user.attendances.build(attendance_day: d)
-        @attendance1.save
+    # 管理者のみ全ユーザーの勤怠編集ページに遷移可能
+    if current_user.admin? || current_user?(@user)
+      #@attendance = Attendance.find(params[:id])
+      @current_day = Date.new(Date.today.year, Date.today.month, Date.today.day)
+      @last_month = @current_day.last_month
+      @next_month = @current_day.next_month
+      @first_day = @current_day.beginning_of_month
+      @last_day = @current_day.end_of_month
+      @week = %w(日 月 火 水 木 金 土 )
+      
+      (@first_day..@last_day).each do |d|
+        if not @user.attendances.any?{|a| a.attendance_day == d}
+          #@attendance = Attendance.new(attendance_day: d, user_id: @user.id) 以下の@attendanceと同意味
+          @attendance1 = @user.attendances.build(attendance_day: d)
+          @attendance1.save
+        end
+        @date = @user.attendances.where("attendance_day >= ? and attendance_day <= ?", @first_day, @last_day)
       end
-      @date = @user.attendances.where("attendance_day >= ? and attendance_day <= ?", @first_day, @last_day)
-    end
-    
-    # 勤怠B：在社時間と在社時間の合計、出勤日数　在社時間@company_timeはviewでは使用しない
-    @date.each do |date|
-      if date.beginning_time != nil && date.leaving_time != nil
-        @company_time = date.leaving_time - date.beginning_time
-        #@start_company_time = 0
-        @total_company_time = (@total_company_time.to_f + @company_time)
-        @attendance_count = @user.attendances.where("beginning_time != ? and leaving_time != ?", nil?, nil?).count
+      
+      # 勤怠B：在社時間と在社時間の合計、出勤日数　在社時間@company_timeはviewでは使用しない
+      @date.each do |date|
+        if date.beginning_time != nil && date.leaving_time != nil
+          @company_time = date.leaving_time - date.beginning_time
+          #@start_company_time = 0
+          @total_company_time = (@total_company_time.to_f + @company_time)
+          @attendance_count = @user.attendances.where("beginning_time != ? and leaving_time != ?", nil?, nil?).count
+        end
       end
+    else
+      flash[:warning] = "他ユーザーの編集ページへ遷移することはできません。"
+      redirect_to current_user
     end
   end
   
@@ -54,7 +61,8 @@ class AttendancesController < ApplicationController
     attendance_params.each do |at, bt|
       @attendance = @user.attendances.find(at)
       #binding.pry
-      if current_user.admin? && current_user && @attendance.attendance_day.future?
+      if current_user.admin? && @attendance.attendance_day.future?
+      elsif current_user?(@user) && @attendance.attendance_day.future?
       elsif bt["beginning_time"].blank? && bt["leaving_time"].blank?
       elsif bt["beginning_time"].blank? || bt["leaving_time"].blank?
         flash[:danger] = "出勤時間または退勤時間が空欄です。"
@@ -73,7 +81,7 @@ class AttendancesController < ApplicationController
   
     # 勤怠B：Strong Parameters fields_forに伴い、user時のコードに工夫が必要
     def attendance_params
-      params.permit(attendances: [:beginning_time, :leaving_time, :attendance_day])[:attendances]
+      params.permit(attendances: [:beginning_time, :leaving_time])[:attendances]
     end
     # def attendance_params
     #   params.require(:attendance).permit(:beginning_time, :leaving_time)
