046881461c41ee201fbad173f7e05f416016d828
Yuki Ueno <yuki.weaver1204@gmail.com>
2019-04-21 06:41:44 +0000

    残業承認2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -132,6 +132,8 @@ class UsersController < ApplicationController
       # 月の勤怠　本日の日付、申請者でレコード取得
       @request_user = OneMonthAttendance.find_by(application_user_id: @user.id, application_date: @current_day)
       
+      # @applicant_user = User.find_by(id: params[:attendance][:user_id])
+      
       
       
       
