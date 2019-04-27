530d7d3c6a847587940c3022ba8cba2fc67764e0
Yuki Ueno <yuki.weaver1204@gmail.com>
2019-04-27 12:37:00 +0000

    拠点編集アコーディオン修正

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,9 @@
 class Attendance < ApplicationRecord
+  # 残業申請　申請状況
+  enum application_status: { "なし" => 1, "申請中" => 2, "承認" => 3, "否認" => 4 }
+  # 勤怠申請　状況
+  enum attendance_application_status: { "なし" => 1, "申請中" => 2, "承認" => 3, "否認" => 4 }
+
   #勤怠B：userと紐付け。attendanceがuserに所属。
   belongs_to :user
   # validates :scheduled_end_time, presence: true
--- app/models/one_month_attendance.rb
+++ app/models/one_month_attendance.rb
@@ -1,2 +1,4 @@
 class OneMonthAttendance < ApplicationRecord
+  # 月の勤怠申請　状況
+  enum one_month_application_status: { "なし" => 1, "申請中" => 2, "承認" => 3, "否認" => 4 }
 end
